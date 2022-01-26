28a6f90262c56c75936cd50bc91b0994fc9b5832
Shane McLeod <shane.mcleod@icloud.com>
2022-01-26 10:52:58 -0800

    test commit from desktop

--- Tests/webcam.py
+++ Tests/webcam.py
@@ -4,9 +4,10 @@ capture = cv.VideoCapture(0)
 
 while True:
     isTrue, frame = capture.read()
-    #canny = cv.Canny(frame, 125, 175)
+    canny = cv.Canny(frame, 125, 175)
     lab = cv.cvtColor(frame, cv.COLOR_BGR2LAB)
     cv.imshow('Video', lab)
+    cv.imshow('Canny', canny)
     cv.imshow('Video Source', frame)
     if cv.waitKey(20) & 0xFF==ord('d'):
         break
